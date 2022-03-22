bf6fd58ab198ce3af38dc9d7d8184b8a01e80e5f
Irem Gunay <iremguunay@gmail.com>
2022-03-22 17:00:16 +0300

    Interface example

---
+++ src/AccountTransferEvent.java
@@ -0,0 +1,25 @@
+import java.sql.Timestamp;
+
+public class AccountTransferEvent implements Event{
+
+    private final Long createdTimestamp;
+    private final String id;
+
+    public AccountTransferEvent(String id) {
+        this.createdTimestamp = new Timestamp(System
+                .currentTimeMillis()).getTime();
+        this.id = id;
+    }
+
+    @Override
+    public Long getTimeStamp() {
+        return this.createdTimestamp;
+    }
+
+    @Override
+    public void process() {
+        System.out.println("Customer " + id + " needs to transfer " +
+                "their service. Reaching out to CTE to remove service form old " +
+                " device and add service to new device.");
+    }
+}
